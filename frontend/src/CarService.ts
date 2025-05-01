3a99c7e3d401e2f6984c94cd0a401a507f53d8d1
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-05-01 14:36:56 -0500

    post car service buildout

--- frontend/src/CarService.ts
+++ frontend/src/CarService.ts
@@ -8,3 +8,10 @@ export const fetchCars: FetchCars = () => (
     axios.get('/api/car')
         .then((r: AxiosResponse<Car[]>) => r.data)
 )
+
+type AddCar = (newCar:Car) => Promise<Car>;
+
+export const addCar:AddCar = (newCar:Car) => (
+    axios.post('api/car', newCar)
+        .then((r:AxiosResponse<Car>) => r.data)
+)
--- frontend/src/__tests__/CarService.test.tsx
+++ frontend/src/__tests__/CarService.test.tsx
@@ -3,7 +3,7 @@ import {setupServer} from "msw/node";
 import axios from "axios";
 import {http, HttpResponse} from "msw";
 import {Car} from "../types.ts";
-import {fetchCars} from "../CarService.ts";
+import {addCar, fetchCars} from "../CarService.ts";
 
 describe('Car Service', () => {
 
@@ -14,36 +14,67 @@ describe('Car Service', () => {
     afterAll(() => server.close())
     afterEach(() => server.resetHandlers())
 
+    it('should post a new car', async () => {
+        const car1:Car = {
+            id: null,
+            make: "Tesla",
+            model: "Model3",
+            year: 2024,
+            price: 40000,
+            isUsed: true
+        }
+
+        const carReciept:Car = {
+            id: 1,
+            make: "Tesla",
+            model: "Model3",
+            year: 2024,
+            price: 40000,
+            isUsed: true
+        }
+
+        server.use(http.post('api/car', () =>
+            HttpResponse.json(carReciept, {status: 201})
+        ))
+
+        expect(await addCar(car1)).toStrictEqual(carReciept);
+
+    });
+
+
     it('should fetch list of cars', async () => {
-        
         const expected: Car[] = [
-            
             {
                 id: null,
                 make: "Tesla",
                 model: "Model3",
                 year: 2024,
                 price: 40000,
-                isUsed: true },
+                isUsed: true
+            },
 
-            {id:1,
+            {
+                id: 1,
                 make: "Kia",
                 model: "Stinger",
                 year: 2018,
                 price: 150000,
-                isUsed: true },
+                isUsed: true
+            },
 
-            {id:2,
+            {
+                id: 2,
                 make: "BMW",
                 model: "X3",
                 year: 2021,
                 price: 50000,
-                isUsed: false }
+                isUsed: false
+            }
         ];
-        
-        server.use(http.get('api/cars', () =>
-        HttpResponse.json(expected, {status: 201})
-))
+
+        server.use(http.get('api/car', () =>
+            HttpResponse.json(expected, {status: 200})
+        ))
         expect(await fetchCars()).toStrictEqual(expected);
 
     });
